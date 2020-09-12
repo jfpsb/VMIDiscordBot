420cc8f49627eadc41c2ab53a4c6129e40dbbd1e
jfpsb <jfpsb_felipe2@hotmail.com>
2020-09-11 21:12:14 -0300

    Arrumando alguns erros no texto

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ Bot.on('message', message => {
 
     switch (message.content) {
         case "!troca":
-            channel.send("1. Abra o módulo de administração do sistema. Ele possui este ícone", { files: ["imagens/iconeadmin.png"] });
+            channel.send("1. Primeiro você irá inserir a devolução do(s) produto(s). Abra o módulo de administração do sistema. Ele possui este ícone", { files: ["imagens/iconeadmin.png"] });
             channel.send("2. Insira seu usuário e senha (o mesmo que você usa para abrir caixa)", { files: ["imagens/telaloginadmin.png"] });
             channel.send("3. No menu \"Estoque\", aperte no botão \"Trocas/Devoluções\"", { files: ["imagens/menuestoquebotaotroca.png"] });
             channel.send("4. A tela de devolução irá abrir. Para selecionar o produto que está sendo devolvido, aperte na lupa ao lado de \"Cód. de Barras\"", { files: ["imagens/telatroca.png"] });
             channel.send("5. Você pode pesquisar produtos por código de barras ou descrição. Aperte em cima do produto que está sendo devolvido e depois aperte em \"F5 - Lançar Produto\"", { files: ["imagens/pesquisaprodutoemtroca.png"] });
             channel.send("6. Após selecionar o produto, escreva a quantidade sendo devolvida no campo \"Quantidade\".\n" +
                         "7. Se possuir, informe o número do cupom da venda do produto que está sendo devolvido. Caso não tenha o número, deixe escrito \"0\" (zero).\n" +
-                        "8. Em \"Gerar Crédito p/ Cliente?\" aperte na opção \"Sim\" e depois aperte na lupa para escolher o cliente (se não marcar \"Sim\" a troca não irá ser efetuada", {files: ["imagens/telatrocaescolhercliente.png"]});
+                        "8. Em \"Gerar Crédito p/ Cliente?\" aperte na opção \"Sim\" e depois aperte na lupa para escolher o cliente (se não marcar \"Sim\" a troca não irá ser efetuada)", {files: ["imagens/telatrocaescolhercliente.png"]});
             channel.send("9. Escolha o cliente com nome \"CLIENTE CRÉDITO\". Clique nele com o mouse e depois clique em \"Ok\"", {files: ["imagens/telaescolherclienteemtroca.png"]});
             channel.send("10. Cheque se os dados estão corretos e então aperte em \"Salvar\"", {files: ["imagens/telatrocaconfirmedados.png"]});
             channel.send("11. Após apertar em \"Salvar\", a devolução deve aparecer na lista", {files: ["imagens/telatrocaefetuadasucesso.png"]});
